9fcc5634b43d1c3f01305201870cdeb8178cd72d
Mickael Maison <mickael.maison@gmail.com>
2018-09-02 12:18:03 +0100

    WIP

--- vagrant/boot_cluster.sh
+++ vagrant/boot_cluster.sh
@@ -17,7 +17,7 @@ while ! nc -q 1 localhost 21805 </dev/null; do echo "Waiting"; sleep 1; done
 # Launch and wait for Kafka
 for i in 1 2 3 4 5; do
     KAFKA_PORT=`expr $i + 9090`
-    export KAFKA_LOG4J_OPTS="-Dkafka.logs.dir=${REPOSITORY_ROOT}/kafka${i} -Dlog4j.configuration=file:log4j.properties"
+    export KAFKA_LOG4J_OPTS="-Dkafka.logs.dir=${REPOSITORY_ROOT}/kafka${i} -Dlog4j.configuration=file:config/log4j.properties"
     cd ${KAFKA_INSTALL_ROOT}/kafka-${KAFKA_PORT} && bin/kafka-server-start.sh -daemon config/server.properties
 done
 while ! nc -q 1 localhost 29095 </dev/null; do echo "Waiting"; sleep 1; done
